45e0f9d51103214df12619499eae3f71471ba7e4
rdomadia <57210345+rdomadia@users.noreply.github.com>
2025-12-30 15:44:05 +0530

    Create Interaction.js

---
+++ JavaScript Fundamentals/Interaction.js
@@ -0,0 +1,9 @@
+// alert
+alert("hello");
+// prompt
+let age = prompt('How old are you?', 100); // default is 100
+console.log(age);
+
+// confirm
+let result = confirm('How are you?'); // Shows Ok cancel
+console.log(result);
